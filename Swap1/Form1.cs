e4178caadf4fdc19daa2206f255bd7079af7579b
Taima <Taima@DESKTOP-GTU61GQ>
2023-11-04 14:44:56 +0300

    Add project files.

---
+++ Swap1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swap1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string First = txtName.Text;
+            string Family = txtFamily.Text;
+            txtName.Text = Family;
+            txtFamily.Text = First;
+        }
+    }
+}
